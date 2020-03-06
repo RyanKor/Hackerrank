d7836d0e550075beed2d5eff3e88c2e298a9d1fb
RyanKor <equuse3144@gmail.com>
2020-03-06 19:00:15 +0700

    2020-03-06 Between two sets, service lane algorithms solved. but I do not understand the logic of algorithms here

---
+++ problem_solving/2020-03-06-Between-Two-Sets.py
@@ -0,0 +1,92 @@
+"""
+You will be given two arrays of integers and asked to determine all integers that satisfy the following two conditions:
+
+The elements of the first array are all factors of the integer being considered
+The integer being considered is a factor of all elements of the second array
+These numbers are referred to as being between the two arrays. You must determine how many such numbers exist.
+
+For example, given the arrays  and , there are two numbers between them:  and . , ,  and  for the first value. Similarly, ,  and , .
+
+Function Description
+
+Complete the getTotalX function in the editor below. It should return the number of integers that are betwen the sets.
+
+getTotalX has the following parameter(s):
+
+a: an array of integers
+b: an array of integers
+Input Format
+
+The first line contains two space-separated integers,  and , the number of elements in array  and the number of elements in array .
+The second line contains  distinct space-separated integers describing  where .
+The third line contains  distinct space-separated integers describing  where .
+
+Constraints
+
+Output Format
+
+Print the number of integers that are considered to be between  and .
+
+Sample Input
+
+2 3
+2 4
+16 32 96
+Sample Output
+
+3
+Explanation
+
+2 and 4 divide evenly into 4, 8, 12 and 16.
+4, 8 and 16 divide evenly into 16, 32, 96.
+
+4, 8 and 16 are the only three numbers for which each element of a is a factor and each is a factor of all elements of b.
+
+이 문제가 이렇게 어려운 문제였던가? 왜 이렇게 버벅대지? 개념은 이해했는데, 코드로 전혀 따라칠 수가 없네
+
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+
+def getTotalX(a, b):
+    nmax, nmin, count = max(a), min(b), 0
+    for i in range(1, int(nmin/nmax)+1):
+        if(sum((i*nmax) % n for n in a)+sum(n % (i*nmax) for n in b)) == 0:
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()

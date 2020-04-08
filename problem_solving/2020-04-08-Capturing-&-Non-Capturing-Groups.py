b22c3fabc92fe9bd11ee031e3970e5c65133c05c
RyanKor <equuse3144@gmail.com>
2020-04-08 10:07:10 +0700

    2020-04-08 capturing & non-capturing groups regular expression algorithm solved

---
+++ problem_solving/2020-04-08-Capturing-&-Non-Capturing-Groups.py
@@ -0,0 +1,28 @@
+"""
+Parenthesis ( ) around a regular expression can group that part of regex together. This allows us to apply different quantifiers to that group.
+
+These parenthesis also create a numbered capturing. It stores the part of string matched by the part of regex inside parentheses.
+
+These numbered capturing can be used for backreferences. ( We shall learn about it later )
+
+ach16.png
+
+
+(?: ) can be used to create a non-capturing group. It is useful if we do not need the group to capture its match.
+
+Task
+
+You have a test String .
+Your task is to write a regex which will match  with the following condition:
+
+ should have  or more consecutive repetitions of ok.
+Note
+
+This is a regex only challenge. You are not required to write a code.
+You have to fill the regex pattern in the blank (_________).
+"""
+import re
+Regex_Pattern = r'(ok){3,}'  # Do not delete 'r'.
+
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())

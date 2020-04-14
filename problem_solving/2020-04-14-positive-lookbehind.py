333fb723015f748813939eb8f896cf3f9c2f25ae
RyanKor <equuse3144@gmail.com>
2020-04-14 11:54:45 +0700

    2020-04-14 positive lookbehind regular expression solved

---
+++ problem_solving/2020-04-14-positive-lookbehind.py
@@ -0,0 +1,26 @@
+"""
+The positive lookbehind (?<=) asserts regex_1 to be immediately preceded by regex_2. Lookbehind is excluded from the match (do not consume matches of regex_2), but only assert whether a match is possible or not.
+
+ach24.png
+
+Task
+
+You have a test String .
+Write a regex which can match all the occurences of digit which are immediately preceded by odd digit.
+
+Note
+
+This is a regex only challenge. You are not required to write a code.
+You have to fill the regex pattern in the blank (_________).
+
+JavaScript do not support lookbehind.
+"""
+import re
+Regex_Pattern = r"(?<=[13579])\d"  # Do not delete 'r'.
+
+
+Test_String = input()
+
+match = re.findall(Regex_Pattern, Test_String)
+
+print("Number of matches :", len(match))
